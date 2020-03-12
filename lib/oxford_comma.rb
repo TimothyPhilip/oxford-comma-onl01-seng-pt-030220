9270b65427031a9a200635e96a520e3530e7b271
Philippe Timothe <philippetimothe@gmail.com>
2020-03-12 18:21:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
-  if array.length < 2 
-    array.join()
+  if array.length <= 2 
+    array.join(" and ")
   else
   last = array.pop()
   array.join(', ') + ', and ' + last
